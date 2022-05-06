cc6315b7931bcd6e0d0d8289a5c4a951e7bb6529
Tismo <deywostismo@gmail.com>
2022-05-06 23:23:50 +0200

    Retrait interface et Correction d'erreur dans nom des fichiers

--- main.cpp
+++ main.cpp
@@ -1,11 +1,4 @@
-#include "pacman.h"
-
+#include "mainwindow.h"
 #include <QApplication>
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    Pacman w;
-    w.show();
-    return a.exec();
-}
+
