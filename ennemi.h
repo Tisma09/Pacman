7e81110d61224baa0b86ad910d6a11c6e38c5753
Tismo <deywostismo@gmail.com>
2022-05-23 08:38:26 +0200

    Petite Correction

--- ennemi.h
+++ ennemi.h
@@ -8,8 +8,6 @@
 #include "regularmove.h"
 
 class Ennemi : public RegularMove, public ObserverCollision, public NotifierCollision{
-private:
-    int pos_x, pos_y;
 public:
     Ennemi(int x, int y, Background* parent);
     virtual void new_pos()=0;
